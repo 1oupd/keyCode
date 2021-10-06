bb2a668187ae0f6154ba39ef87e0ad17e601f37e
danny vincent <87720409+1oupd@users.noreply.github.com>
2021-10-06 17:54:11 +0100

    updated app.js, added functioning keyboard events

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ document.addEventListener('keydown', (event) => {
 
     let num = document.getElementById("num")
     let location = document.getElementById("location")
+    let key = document.getElementById("key")
+    let which = document.getElementById("which")
     let kcode = document.getElementById("code")
 
     let keyPress = event.key;
     num.textContent = (keyPress);
 
-    let keyLoc = event.location;
-    location.textContent = (keyLoc);
+    let keyL = event.key;
+    key.textContent = (keyL);
+
+    let klocation = event.location;
+    location.textContent = (klocation);
     
-    let keyCodes = event.which;
+    let keyWhich = event.which;
+    which.textContent = (keyWhich);
+
+    let keyCodes = event.code;
     kcode.textContent = (keyCodes);
 });
